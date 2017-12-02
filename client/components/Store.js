97d4cbbcdebcfc12c2cad851923d96e2bb6d0221
Ajay Gandhi <aag255@cornell.edu>
2017-12-02 11:02:15 -0800

    Add player state to store

--- client/components/Store.js
+++ client/components/Store.js
@@ -1,17 +1,35 @@
 import { createStore } from "redux";
 
+const REPEAT_STATE = Object.freeze({
+  off: 0,
+  all: 1,
+  one: 2,
+});
+
+const initialiState = {
+  searchQuery: "",
+  repeat: REPEAT_STATE.off,
+  shuffle: false,
+  rawList: [],
+  queue: [],
+};
+
 const setSearchQuery = (searchQuery) => {
   return {
     type: "SET_SEARCH",
     searchQuery,
   };
 };
+const toggleRepeat = () => {
+  return { type: "TOGGLE_REPEAT" };
+};
+const toggleShuffle = () => {
+  return { type: "TOGGLE_SHUFFLE" };
+};
 const actions = {
   setSearchQuery,
-};
-
-const initialiState = {
-  searchQuery: "",
+  toggleRepeat,
+  toggleShuffle,
 };
 
 const reducer = (state = initialiState, action) => {
@@ -20,6 +38,16 @@ const reducer = (state = initialiState, action) => {
       return Object.assign({}, state, {
         searchQuery: action.searchQuery,
       });
+
+    case "TOGGLE_REPEAT": {
+      let repeat = state.repeat === REPEAT_STATE.off
+        ? REPEAT_STATE.all
+        : (state.repeat === REPEAT_STATE.all ? REPEAT_STATE.one : REPEAT_STATE.off);
+      return Object.assign({}, state, { repeat });
+    }
+
+    case "TOGGLE_SHUFFLE":
+      return Object.assign({}, state, { shuffle: !state.shuffle });
 
     default:
       return state;
@@ -33,5 +61,6 @@ const store = createStore(
 
 export {
   store,
-  actions
+  actions,
+  REPEAT_STATE,
 };
