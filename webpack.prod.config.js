beaa5bb605e35a50169a682c10bbcc7e4a0e46c7
Ajay Gandhi <aag255@cornell.edu>
2017-12-23 09:59:26 -0800

    Fix typo

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -8,7 +8,7 @@ module.exports = {
   output: {
     path: path.join(__dirname, "public"),
     filename: "bundle.js",
-    public: "/public/",
+    publicPath: "/public/",
   },
   plugins: [
     // new webpack.optimize.DedupePlugin(),
