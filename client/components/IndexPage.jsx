83c14bc485231315d661ec008efab5656367e1b9
Ajay Gandhi <aag255@cornell.edu>
2017-12-22 21:11:29 -0800

    Create SearchBar component and style index page

--- client/components/IndexPage.jsx
+++ client/components/IndexPage.jsx
@@ -1,57 +1,16 @@
+import "./scss/IndexPage.scss";
+
 import React from "react";
-import PropTypes from "prop-types";
-import { connect } from "react-redux";
-import { actions } from "./store/Store";
-import { withRouter, Link } from "react-router-dom";
-import { compose } from "recompose";
+import SearchBar from "./SearchBar";
 
-import { Button } from "react-bootstrap";
+const IndexPage = () => (
+  <div className="IndexPage">
+    <h1 className="IndexPage__heading">YouMuse</h1>
+    <div className="IndexPage__content">
+      <SearchBar className="IndexPage__content__SearchBar" />
+      <h3 className="IndexPage__content__subtitle">Listen to YouTube, ad-free</h3>
+    </div>
+  </div>
+);
 
-class IndexPage extends React.Component {
-  static propTypes = {
-    history: PropTypes.object,
-    searchQuery: PropTypes.string,
-    updateSearchQuery: PropTypes.func,
-  };
-
-  handleSearchChange = e => this.props.updateSearchQuery(e.target.value);
-  handleKeyPress = (e) => {
-    if (e.key === "Enter") {
-      this.props.history.push(`/search/${this.props.searchQuery}`);
-    }
-  }
-
-  render = () => {
-    return (
-      <div>
-        <h1>YouMuse</h1>
-        <input
-          placeholder="Search YouTube..."
-          value={ this.props.searchQuery }
-          onChange={ this.handleSearchChange }
-          onKeyPress={ this.handleKeyPress }
-        />
-        <Button>
-          <Link to={ `/search/${this.props.searchQuery}` }>Search</Link>
-        </Button>
-        <p>Subtitle</p>
-      </div>
-    );
-  }
-}
-
-const mapStateToProps = (state) => {
-  return {
-    searchQuery: state.searchQuery,
-  };
-};
-const mapDispatchToProps = (dispatch) => {
-  return {
-    updateSearchQuery: searchQuery => dispatch(actions.setSearchQuery(searchQuery)),
-  };
-};
-
-export default compose(
-  withRouter,
-  connect(mapStateToProps, mapDispatchToProps)
-)(IndexPage);
+export default IndexPage;
---
+++ client/components/SearchBar.jsx
@@ -0,0 +1,63 @@
+import "./scss/SearchBar.scss";
+
+import React from "react";
+import PropTypes from "prop-types";
+import { compose } from "recompose";
+import { connect } from "react-redux";
+import { withRouter } from "react-router-dom";
+import { actions } from "./store/Store";
+
+import { Button } from "react-bootstrap";
+import Icon from "./Icon";
+
+class SearchBar extends React.PureComponent {
+  static propTypes = {
+    history: PropTypes.object,
+    className: PropTypes.string,
+    searchQuery: PropTypes.string,
+    updateSearchQuery: PropTypes.func,
+  };
+
+  handleSearchChange = e => this.props.updateSearchQuery(e.target.value)
+  handleKeyPress = (e) => {
+    if (e.key === "Enter") this.handleSearchClick();
+  }
+  handleSearchClick = () => {
+    this.props.history.push(`/search/${this.props.searchQuery}`);
+    this.props.fetchSearchResults();
+  }
+
+  render = () => {
+    return (
+      <div className={ `SearchBar ${this.props.className || ""}` }>
+        <input
+          className="SearchBar__input"
+          placeholder="Search YouTube..."
+          value={ this.props.searchQuery }
+          onChange={ this.handleSearchChange }
+          onKeyPress={ this.handleKeyPress }
+        />
+        <Button className="SearchBar__button BorderlessButton" onClick={ this.handleSearchClick }>
+          <Icon glyph="search" />
+        </Button>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = (state) => {
+  return {
+    searchQuery: state.searchQuery,
+  };
+};
+const mapDispatchToProps = (dispatch) => {
+  return {
+    updateSearchQuery: searchQuery => dispatch(actions.setSearchQuery(searchQuery)),
+    fetchSearchResults: () => dispatch(actions.fetchSearchResults()),
+  };
+};
+
+export default compose(
+  withRouter,
+  connect(mapStateToProps, mapDispatchToProps)
+)(SearchBar);
