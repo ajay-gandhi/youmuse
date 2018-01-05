80ff9a0b2ed40becfd60b0cb51837c72d41d9374
Ajay Gandhi <aag255@cornell.edu>
2018-01-05 11:30:23 -0800

    Fix repeat

--- client/components/store/QueueUtils.js
+++ client/components/store/QueueUtils.js
@@ -1,4 +1,4 @@
-import { MAX_QUEUE_SIZE } from "./constants";
+import { MAX_QUEUE_SIZE, REPEAT_STATE } from "./constants";
 
 export const copyArray = arr => arr.map(item => ({ ...item }));
 
@@ -13,15 +13,19 @@ const shuffleArray = (arr) => {
 
 const playlistToQueue = (playlist, shuffle) => shuffle ? shuffleArray(playlist) : copyArray(playlist);
 export const generateQueue = (playlist, shuffle, repeat) => {
+  if (repeat === REPEAT_STATE.one) return Array(MAX_QUEUE_SIZE).fill({ ...playlist[0] });
+
   let queue = playlistToQueue(playlist, shuffle);
-  while (repeat && queue.length < MAX_QUEUE_SIZE) {
+  while (repeat === REPEAT_STATE.all && queue.length < MAX_QUEUE_SIZE) {
     queue = queue.concat(playlistToQueue(playlist, shuffle));
   }
   return queue;
 };
 export const refillQueue = (queue, playlist, shuffle, repeat) => {
+  if (repeat === REPEAT_STATE.one) return Array(MAX_QUEUE_SIZE).fill({ ...queue[0] });
+
   let newQueue = copyArray(queue);
-  while (repeat && newQueue.length < MAX_QUEUE_SIZE) {
+  while (repeat === REPEAT_STATE.all && newQueue.length < MAX_QUEUE_SIZE) {
     newQueue = newQueue.concat(playlistToQueue(playlist, shuffle));
   }
   return queue;
